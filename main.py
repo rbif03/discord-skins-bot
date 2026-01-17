c233f8591b2ae856dfec6603b98ac4ae8a8683d4
Roger Bif <rbif03@gmail.com>
2026-01-16 21:45:52 -0300

    add dynamodb logic & first commands

--- main.py
+++ main.py
@@ -1,6 +1,53 @@
+import asyncio
 import config
+import discord
+from discord.ext import commands, tasks
+from services.dynamodb import add_guild_to_db, update_guild_channel
+
+
 from services.ssm import get_parameter
 
-parameter_name = config.DISCORD_TOKEN_SSM_PATH
-DISCORD_TOKEN = get_parameter(parameter_name)
-print(DISCORD_TOKEN)
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+PARAMETER_NAME = config.DISCORD_TOKEN_SSM_PATH
+DISCORD_TOKEN = get_parameter(PARAMETER_NAME)
+COMMAND_PREFIX = "->"
+
+bot = commands.Bot(command_prefix=COMMAND_PREFIX, intents=intents)
+
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user.name} - {bot.user.id}")
+    print("------")
+
+
+@bot.event
+async def on_guild_join(guild: discord.Guild):
+    await asyncio.to_thread(add_guild_to_db, guild.id)
+    print(f"Bot joined guild {guild.id}")
+
+
+@bot.command()
+async def set_skinsbot_channel(ctx: commands.Context):
+    guild_obj = ctx.guild
+    channel_obj = ctx.channel
+    try:
+        await asyncio.to_thread(update_guild_channel, guild_obj.id, channel_obj.id)
+        await channel_obj.send(
+            f":white_check_mark: The channel '{channel_obj.name}' will now receive skin prices updates!"
+        )
+    except Exception as e:
+        await channel_obj.send(f":cross_mark: Failed to change channel. {e}")
+
+
+@bot.command()
+async def add_skin(ctx: commands.Context):
+    head = f"{ctx.prefix}{ctx.invoked_with}"
+    message = ctx.message.content[len(head) :].strip()
+    print(message)
+
+
+bot.run(DISCORD_TOKEN)
---
+++ services/dynamodb.py
@@ -0,0 +1,32 @@
+import boto3
+
+import boto3
+from boto3.dynamodb.conditions import Key, Attr
+
+dynamodb = boto3.resource("dynamodb")
+guild_info_table = dynamodb.Table("skinsbot.guild_info")
+
+
+def add_guild_to_db(guild_id: int):
+    # Checks if guild is already in DB, if not, adds it
+    response = guild_info_table.query(
+        KeyConditionExpression=Key("guild_id").eq(guild_id)
+    )
+
+    if not response.get("Items"):
+        guild_info_table.put_item(Item={"guild_id": guild_id})
+
+
+def update_guild_channel(guild_id: int, channel_id: int):
+    add_guild_to_db(guild_id)  # if the guild isn't in DB for some reason, adds it
+
+    guild_info_table.update_item(
+        Key={"guild_id": guild_id},
+        UpdateExpression="SET channel_id = :channel_id",
+        ExpressionAttributeValues={":channel_id": channel_id},
+    )
+
+
+if __name__ == "__main__":
+    # add_guild_to_db(123)
+    update_guild_channel(123, 789)
